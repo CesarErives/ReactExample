17376c32b9f7bae28a58778047a57656a6b8d47c
CesarErives <a6519150012@utch.edu.mx>
2021-05-26 00:25:29 -0600

    Add the browserrouter to specify routes of pages

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import React from 'react';
+import {BrowserRouter,Route,Switch} from "react-router-dom"
 import Layout from './pages/Layout/Layout'
 import Main from './pages/main/Main'
 import NewBadge from "./pages/NewBadge/NewBadge"
 function App() {
   return (
 
-    <React.Fragment>
-        <NewBadge>
-          
-        </NewBadge>
-    </React.Fragment>
+    <BrowserRouter>
+    <Layout>
+        <Switch>
+          <Route exact path="/" component={Main}></Route>
+      {/* <Route exact path="/AllBadges" component={AllBadges}></Route>
+          <Route exact path="/signup" component={SignUp}></Route>
+          <Route exact path="/login" component={Login}></Route> */}
+          <Route exact path="/new" component={NewBadge}></Route>
+     {/*     <Route component={PathNotFound}></Route> */}
+        </Switch>
+    </Layout>
+    </BrowserRouter>
 
   );
 }
