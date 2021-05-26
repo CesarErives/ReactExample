66721481e68fec7536b3dda8a3bca48855332cfb
CesarErives <a6519150012@utch.edu.mx>
2021-05-25 18:02:56 -0600

    Changed badge.js to be dynamic

--- src/components/Badge.js
+++ src/components/Badge.js
@@ -1,11 +1,5 @@
 import React, { Component } from 'react'
-import HeaderBackground from '../images/bg-pattern-card.svg'
-import ProfilePic from '../images/image-victor.jpg'
 import './style/Badge.css'
-
-
-
-
 class Badge extends Component {
 
     render() {
@@ -13,29 +7,29 @@ class Badge extends Component {
             <React.Fragment>
             <div className="Badge m-5 flex-column">
                 <div className="Badge__header">
-                    <img src={HeaderBackground} alt="header_backgrond" />
+                    <img src={this.props.HeaderBackground} alt="header_backgrond" />
 
                 </div>
                 <div className="container mt-5 mb-1 Badge__userInfo container ">
                     <div className="Badge__userImage">
-                        <img src={ProfilePic} alt="Profile Pic" />
+                        <img src={this.props.ProfilePic} alt="Profile Pic" />
                     </div>
-                    <h4 className='text-center mt-5'>Victor BS <i>25</i></h4>
-                    <p className='text-center mt'>Las Vegas</p>
+                    <h4 className='text-center mt-5'>{this.props.name} <i>{this.props.age}</i></h4>
+                    <p className='text-center mt'>{this.props.city}</p>
                 </div>
                 <div className="Badge__info container pt-3">
                 <div className="row">
                     <div className="col">
-                    <h4 className="text-center fw bold">80k</h4>
+                    <h4 className="text-center fw bold">{this.props.games}</h4>
+                    <p className="text-center">Games</p>
+                    </div>
+                    <div className="col">
+                    <h4 className="text-center fw bold">{this.props.achievements}</h4>
+                    <p className="text-center">Achievements</p>
+                    </div>
+                    <div className="col">
+                    <h4 className="text-center fw bold">{this.props.followers}</h4>
                     <p className="text-center">Followers</p>
-                    </div>
-                    <div className="col">
-                    <h4 className="text-center fw bold">150k</h4>
-                    <p className="text-center">Likes</p>
-                    </div>
-                    <div className="col">
-                    <h4 className="text-center fw bold">300</h4>
-                    <p className="text-center">Pictures</p>
                     </div>
                 </div>
                 </div>
