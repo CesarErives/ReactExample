4402b091f630d3d289ab2039fa27d4c70ff5ceee
CesarErives <a6519150012@utch.edu.mx>
2021-05-26 11:46:47 -0600

    Built new page to display the login form

---
+++ src/pages/Login/login.js
@@ -0,0 +1,49 @@
+import React from "react"
+import LoginForm from "../../components/LoginForm"
+import api from "../../libs/api"
+import "./login.css"
+
+class login extends React.Component{
+    handleChange= event =>{
+        this.setState({
+            form:{
+                ...this.state.form,[event.target.name]: event.target.value
+            }
+        })
+    }
+
+    handleSubmit= async(event)=>{
+        event.preventDefault();
+        this.setState({loading:true, error:null})
+        try{
+            await api.badges.create(this.state.form)
+            this.setState({loading:false,error:null})
+            this.props.history.push("/")
+        }catch(error){
+            this.setState({loading:false,error:error})
+        }
+    }
+
+    state = {
+        loading : false,
+        error: null,
+        form:{
+            Email: "",
+            Password: "",
+        }
+    }
+
+    render(){
+        return(
+            <React.Fragment>
+    <LoginForm 
+    onChange={this.handleChange}      
+    onSubmit={this.handleSubmit} 
+    formValues={this.state.form}>
+    </LoginForm>
+            </React.Fragment>
+        );
+    }
+}
+
+export default login
