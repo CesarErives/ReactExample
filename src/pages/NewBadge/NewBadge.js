a3c742150ef6d39065415c274f6c3e21585fa01d
CesarErives <a6519150012@utch.edu.mx>
2021-05-26 08:58:57 -0600

    added the functionality to save json file

--- src/pages/NewBadge/NewBadge.js
+++ src/pages/NewBadge/NewBadge.js
@@ -2,6 +2,7 @@ import React from "react"
 import Hero from "../../components/Hero"
 import Badge from "../../components/Badge"
 import BadgeForm from"../../components/BadgeForm"
+import api from "../../libs/api"
 import "./NewBadge.css"
 
 class NewBadge extends React.Component{
