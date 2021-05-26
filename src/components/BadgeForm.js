18dc0f19c194cbe282d5bf3f4529193c9fa45b1e
CesarErives <a6519150012@utch.edu.mx>
2021-05-25 22:55:56 -0600

    Added a form to fill badge

---
+++ src/components/BadgeForm.js
@@ -0,0 +1,16 @@
+import React from "react"
+import "./style/BadgeForm.css"
+
+class BadgeForm extends React.Component{
+    render(){
+        return(
+            <React.Fragment>
+                <h1>Form to fill your Profile</h1>
+                
+            </React.Fragment>
+
+        );
+    }
+}
+
+export default BadgeForm
