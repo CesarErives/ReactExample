d1bd587b8002fa437d1f498fed4b2e428144f9bf
CesarErives <a6519150012@utch.edu.mx>
2021-05-25 23:27:26 -0600

    Built form to fullfil data

--- src/components/BadgeForm.js
+++ src/components/BadgeForm.js
@@ -6,7 +6,46 @@ class BadgeForm extends React.Component{
         return(
             <React.Fragment>
                 <h1>Form to fill your Profile</h1>
-                
+                <div className="BadgeForm m-4">
+                    <form onSubmit={this.props.onSubmit}>
+                    <div className="form-group mb-2">
+                            <label className="mb-1">
+                                Cover Picture URL:
+                            </label>
+                            <input type="text" className="form-control" name="HeaderBackground" onChange={this.props.onChange}
+                            value={this.props.formValues.HeaderBackground}/>
+                        </div>
+                        <div className="form-group mb-2">
+                            <label className="mb-1">
+                                Profile Picture URL:
+                            </label>
+                            <input type="text" className="form-control" name="ProfilePic" onChange={this.props.onChange}
+                            value={this.props.formValues.ProfilePic}/>
+                        </div>
+                        <div className="form-group mb-2">
+                            <label className="mb-1">
+                                Name:
+                            </label>
+                            <input type="text" className="form-control" name="name" onChange={this.props.onChange}
+                            value={this.props.formValues.name}/>
+                        </div>
+                        <div className="form-group mb-2">
+                            <label className="mb-1">
+                                Age:
+                            </label>
+                            <input type="text" className="form-control" name="age" onChange={this.props.onChange}
+                            value={this.props.formValues.age}/>
+                        </div>
+                        <div className="form-group mb-2">
+                            <label className="mb-1">
+                                City:
+                            </label>
+                            <input type="text" className="form-control" name="city" onChange={this.props.onChange}
+                            value={this.props.formValues.city}/>
+                            <button type="submit" className="saveChangesbtn">Save your changes</button>
+                        </div>
+                    </form>
+                </div>
             </React.Fragment>
 
         );
