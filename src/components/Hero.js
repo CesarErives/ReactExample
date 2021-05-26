ec6f55e66d33c1196cc8d3c9eb3efe59bf46eb09
CesarErives <a6519150012@utch.edu.mx>
2021-05-25 19:23:19 -0600

    Fixed hero not displaying correctly

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -6,10 +6,9 @@ const Hero = props =>{
     return(
         <React.Fragment>
             <div className="Hero">
-                <div className="Hero__color_filter" style={{height: props.h}}>
+                <div className="Hero__color_filter" style={{height: props.h}}></div>
                     <div className="Hero__img" style={{height: props.h}}>
                         <img src={HeroImage} alt="" />
-                    </div>
                 </div>
             </div>
         </React.Fragment>
